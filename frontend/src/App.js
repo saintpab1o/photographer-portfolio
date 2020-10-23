3b549af6d690f9629bd9e88d73738ed05d075a5a
saintpablo <pau1magic@aim.com>
2020-10-23 19:53:25 -0400

    Completed Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
 
 
 class App extends React.Component{
   render(){
-    return <div>Hey From React</div>
+    return (
+      <Header/>
+      )
   }
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../css/Header.css';
+
+class Header extends React.Component{
+    render(){
+        return (
+          <div id="header-container" className="ui clearing segment">
+            <div id="header-text-container">
+            <h3 id="header-icon" class="ui icon header">
+              <i class="photo icon"></i>
+            </h3>
+              <h3 id="header-text" className="ui right floated header">
+                About
+              </h3>
+              <h3 id="header-text" className="ui right floated header">
+                Contact
+              </h3>
+              <h3 id="header-text" className="ui right floated header">
+                Booking
+              </h3>
+              <h3 id="header-text" className="ui right floated header">
+                Catalog
+              </h3>
+            </div>
+          </div>
+        );
+    }   
+}
+
+export default Header;
