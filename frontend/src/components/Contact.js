f7dcdc7c0eb7edcf68e7822a20a3ac39c8001921
saintpablo <pau1magic@aim.com>
2020-10-27 23:19:32 -0400

    Filled in boilerplate header

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -17,8 +17,9 @@ class Contact extends React.Component{
                   customized to specifactions. Thanks for stopping by!
                 </p>
                 <div className="contact">
-                    <a href="#"><i class="fas fa-phone"></i> 631-555-5555</a>
-                    <a href="#"><i class="fas fa-envelope"></i> pm@pmshoots.com</a>
+                    <i class="fas fa-phone">631-555-9595</i>
+                    <br></br>
+                   <i class="fas fa-envelope">pm@pmshoots.com</i>
                     </div>
 
                   <div className="socials">
@@ -38,18 +39,30 @@ class Contact extends React.Component{
 
               </div>
 
-              <div className="footer-section links">
+              <div id="footer-links" className="footer-section links">
                   <h1>Quick Links</h1>
                   <br></br>
                   <ul>
-                      <a href="#"><li>About</li></a>
-                      <a href="#"><li>Catalog</li></a>
-                      <a href="#"><li>Booking</li></a>
-                      <a href="#"><li>More</li></a>
+                    <li>About</li>
+                    <li>Events</li>
+                    <li>Catalog</li>
+                    <li>More</li>
+                
                   </ul>
               </div>
 
-              <div className="footer-section contact-form"></div>
+              <div className="footer-section contact-form">
+                  <h1>Contact</h1>
+                  <br></br>
+                  <form action="index.html" method="post">
+                      <input className="text-input contact-input"type="email" name="email" placeholder="email"></input>
+                    <textarea name="message"  className="text-inputer contact-input" placeholder="Place your request here!"></textarea>
+                    <button type="submit" class="btn btn-big">
+                        <i class="fas fa-envelope"></i>
+                        Send
+                    </button>
+                  </form>
+              </div>
             </div>
 
             <div className="footer-bottom">
