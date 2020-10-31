32b27e9b4f3df2f7ef8c88deb9d182dfb725e21e
saintpablo <pau1magic@aim.com>
2020-10-31 00:31:55 -0400

    Created Responsive Navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,53 +2,63 @@ import React from 'react';
 import '../css/Header.css';
 
 
-class Header extends React.Component{
-  
+class Header extends React.Component {
 
-    // toggleClass = () =>{
-    
-    // }
-  
-    render(){
-        return (
-          <header>
-            <div class="logo" >
-              <h1 class="logo-text">
-                <span>Head</span>shots
-              </h1>
-            </div>
-            <i class="fa fa-bars menu-toggle" onClick={this.toggleClass}></i>
-            <ul class="nav">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
+  state = {
+    count: 0,
+    liked: false
+}
 
+
+switchClass = () => {
+        this.refs.navbar.classList.toggle('showing');
+    }
+
+
+
+
+  render() {
+
+    return (
+      <header>
+        <div class="logo">
+          <h1 class="logo-text">
+            <span>Head</span>shots
+          </h1>
+        </div>
+
+        <i class="fa fa-bars menu-toggle" onClick={this.switchClass}></i>
+        <ul ref="navbar" class="nav">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+
+          <li>
+            <a href="#">
+              <i class="fa fa-user"></i> Admin User{" "}
+              <i class="fa fa-chevron-down"></i>
+            </a>
+            <ul>
               <li>
-                <a href="#">
-                  <i class="fa fa-user"></i> Admin User{" "}
-                  <i class="fa fa-chevron-down"></i>
+                <a href="#">DashBoard</a>
+              </li>
+              <li>
+                <a class="logout" href="#">
+                  Logout
                 </a>
-                <ul>
-                  <li>
-                    <a href="#">DashBoard</a>
-                  </li>
-                  <li>
-                    <a class="logout" href="#">
-                      Logout
-                    </a>
-                  </li>
-                </ul>
               </li>
             </ul>
-          </header>
-        );
-    }   
+          </li>
+        </ul>
+      </header>
+    );
+  }
 }
 
 export default Header;
