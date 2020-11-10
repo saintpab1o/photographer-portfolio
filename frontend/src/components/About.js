3647b3c28e023f440748b362d9ffcbce71c4bd64
saintpablo <pau1magic@aim.com>
2020-11-09 22:35:28 -0500

    Basic About Page Created

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
 import Contact from '../components/Contact'
+import '../css/Splash.css';
+import Reme from "../portfolio-images/General/reme_backdrop.jpg";
 
 
 class About extends React.Component{
@@ -8,7 +10,18 @@ class About extends React.Component{
         return(
         <div>
             <Header/>
-            ABOUT PAGE
+            <div className='about-us-text'>
+                <h1>
+                    About Us
+                </h1>
+                <h3>
+                    Welcome to Headshots! New Yorks premier source for photography services, art tours
+                    and artist management.
+                    
+                </h3>
+                    <img className="group-images" src={Reme} alt="reme" />
+
+            </div>
             <Contact/>
         </div>
         )
