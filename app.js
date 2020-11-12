86957f95d3a63e767510d73662ef9f3a3a452982
saintpablo <pau1magic@aim.com>
2020-11-11 20:56:14 -0500

    heroku 4

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const bodyParser = require("body-parser");
 const contact = require('./routes/api/contacts')
+var router = express.Router();
+var nodemailer = require("nodemailer");
+const creds = require("./config");
 const group = require('./routes/api/imageGroups')
 var cors = require("cors");
 const path = require("path");
@@ -20,6 +23,9 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json());
+app.use("/", router);
+app.use(cors());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -35,11 +41,54 @@ app.get("/", (req, res) => {
 
 app.use("/api", contact);
 app.use("/api", group);
+app.use("/api/about", about);
 
-app.get("/about", function (req, res) {
-  res.send("about");
+
+
+var transport = {
+  host: "smtp.gmail.com", // Don’t forget to replace with the SMTP host of your provider
+  port: 587,
+  auth: {
+    user: creds.USER,
+    pass: creds.PASS,
+  },
+};
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take messages");
+  }
 });
 
+router.post("/send", (req, res, next) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `;
+
+  var mail = {
+    from: name,
+    to: "paulmagioncalda@gmail.com", // Change to email address that you want to receive messages on
+    subject: "New Message from Contact Form",
+    text: content,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+      res.json({
+        status: "success",
+      });
+    }
+  });
+});
 
 
 
---
+++ routes/api/about.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+
+// router.get("/groupname", (req, res) => {
+//   res.json({ msg: "fuck you" });
+// });
+
+router.get("/api/about", (req, res) => {
+  About.find()
+    .then((about) => res.json(about))
+    .catch((err) => res.status(404).json({ noaboutfound: "No about found" }));
+});
+
+module.exports = router;
