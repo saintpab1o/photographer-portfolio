ffe21ab81a60e50f96a6e9e8500af414936c1ba0
saintpablo <pau1magic@aim.com>
2020-11-24 01:35:19 -0500

    Updated to Hash Route2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import Splash from './components/Splash';
 // import Catalog from './components/Catalog';
 import Booking from './components/Booking';
 // import Calender from './components/Calender';
-import { BrowserRouter, Route  } from 'react-router-dom';
+import { HashRouter,BrowserRouter, Route , Router  } from 'react-router-dom';
 import Wtfcity from './components/Wtfcity';
 import Halloween from './components/Halloween';
 import { Switch } from "react-router-dom";
@@ -18,16 +18,16 @@ class App extends React.Component{
   render(){
     return (
       <div>
+        
+          <BrowserRouter>
+            <Route exact path="/" component={Splash} />
+            <Route exact path="/about" component={About} />
+            {/* <Route exact path="/catalog" component={Catalog} /> */}
+            <Route exact path="/booking" component={Booking} />
+            <Route exact path="/wtfcity" component={Wtfcity} />
+            <Route exact path="/10.31" component={Halloween} />
+          </BrowserRouter>
       
-        <Switch>
-          <Route exact path="/" component={Splash} />
-          <Route exact path="/about" component={About} />
-          {/* <Route exact path="/catalog" component={Catalog} /> */}
-          <Route exact path="/booking" component={Booking} />
-          <Route exact path="/wtfcity" component={Wtfcity} />
-          <Route exact path="/10.31" component={Halloween}/>
-        </Switch>
-         
       </div>
     );
   }
